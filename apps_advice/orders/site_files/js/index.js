cb9ec55c635208bdd2f6fa633bc28fe90b076927
ahmed2122 <59345293+ahmed2122@users.noreply.github.com>
2021-09-06 18:25:07 +0200

    last ui edit

--- apps_advice/orders/site_files/js/index.js
+++ apps_advice/orders/site_files/js/index.js
@@ -1,67 +1,70 @@
-app.controller("orders", function ($scope, $http, $timeout) {
+app.controller("main", function ($scope, $http, $timeout) {
   $scope._search = {};
 
-  $scope.orders = {};
+  $scope.order = {
+    image: [
+      {
+        name: "/images/no.jpg",
+      },
+    ],
+  };
   $scope.prescription = {};
-  
+
   $scope.displayAddOrders = function () {
-    $scope.error = '';
+    $scope.error = "";
     $scope.orders = {
-      image_url: '/images/orders.png',
+      image_url: "/images/orders.png",
       card_url: "/images/cardImage.png",
       active: true,
-      hasInsurance: false
+      hasInsurance: false,
     };
 
-    site.showModal('#ordersAddModal');
-
+    site.showModal("#ordersAddModal");
   };
 
-  $scope.addOrders = function () {
-    $scope.error = '';
-    const v = site.validated('#ordersAddModal');
+  $scope.confirmOrder = function () {
+    $scope.error = "";
+    /* const v = site.validated("#ordersAddModal");
     if (!v.ok) {
       $scope.error = v.messages[0].ar;
       return;
-    }
-   console.log('##user.ref_info._id##');
-   $scope.orders.user = {
-     _id : '##user.ref_info._id##' 
-   };
+    }*/
+    $scope.order.user = {
+      _id: "##user.ref_info._id##",
+    };
     $scope.busy = true;
     $http({
       method: "POST",
       url: "/api/orders/add",
-      data: $scope.orders
+      data: $scope.order,
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.done) {
-          site.hideModal('#ordersAddModal');
-          $scope.getOrdersList();
+          alert("done");
         } else {
           $scope.error = response.data.error;
-          if (response.data.error.like('*Must Enter Code*')) {
-            $scope.error = "##word.must_enter_code##"
+          if (response.data.error.like("*Must Enter Code*")) {
+            $scope.error = "##word.must_enter_code##";
           }
         }
       },
       function (err) {
         console.log(err);
       }
-    )
+    );
   };
 
   $scope.displayUpdateOrders = function (orders) {
-    $scope.error = '';
+    $scope.error = "";
     $scope.viewOrders(orders);
     $scope.orders = {};
-    site.showModal('#ordersUpdateModal');
+    site.showModal("#ordersUpdateModal");
   };
 
   $scope.updateOrders = function () {
-    $scope.error = '';
-    const v = site.validated('#ordersUpdateModal');
+    $scope.error = "";
+    const v = site.validated("#ordersUpdateModal");
     if (!v.ok) {
       $scope.error = v.messages[0].ar;
       return;
@@ -70,39 +73,39 @@ app.controller("orders", function ($scope, $http, $timeout) {
     $http({
       method: "POST",
       url: "/api/orders/update1",
-      data: $scope.orders
+      data: $scope.orders,
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.done) {
-          site.hideModal('#ordersUpdateModal');
+          site.hideModal("#ordersUpdateModal");
           $scope.getOrdersList();
         } else {
-          $scope.error = 'Please Login First';
+          $scope.error = "Please Login First";
         }
       },
       function (err) {
         console.log(err);
       }
-    )
+    );
   };
 
   $scope.displayDetailsOrders = function (orders) {
-    $scope.error = '';
+    $scope.error = "";
     $scope.viewOrders(orders);
     $scope.orders = {};
-    site.showModal('#ordersViewModal');
+    site.showModal("#ordersViewModal");
   };
 
   $scope.viewOrders = function (orders) {
     $scope.busy = true;
-    $scope.error = '';
+    $scope.error = "";
     $http({
       method: "POST",
       url: "/api/orders/view",
       data: {
-        id: orders.id
-      }
+        id: orders.id,
+      },
     }).then(
       function (response) {
         $scope.busy = false;
@@ -115,31 +118,31 @@ app.controller("orders", function ($scope, $http, $timeout) {
       function (err) {
         console.log(err);
       }
-    )
+    );
   };
 
   $scope.displayDeleteOrders = function (orders) {
-    $scope.error = '';
+    $scope.error = "";
     $scope.viewOrders(orders);
     $scope.orders = {};
-    site.showModal('#ordersDeleteModal');
+    site.showModal("#ordersDeleteModal");
   };
 
   $scope.deleteOrders = function () {
     $scope.busy = true;
-    $scope.error = '';
+    $scope.error = "";
 
     $http({
       method: "POST",
       url: "/api/orders/delete1",
       data: {
-        id: $scope.orders.id
-      }
+        id: $scope.orders.id,
+      },
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.done) {
-          site.hideModal('#ordersDeleteModal');
+          site.hideModal("#ordersDeleteModal");
           $scope.getOrdersList();
         } else {
           $scope.error = response.data.error;
@@ -148,7 +151,7 @@ app.controller("orders", function ($scope, $http, $timeout) {
       function (err) {
         console.log(err);
       }
-    )
+    );
   };
 
   $scope.getOrdersList = function (where) {
@@ -157,24 +160,22 @@ app.controller("orders", function ($scope, $http, $timeout) {
     $http({
       method: "POST",
       url: "/api/orders/search",
-      data: where
+      data: where,
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.docs.length > 0) {
           $scope.list = response.data.docs;
           $scope.count = response.data.totalDocs;
-          site.hideModal('#ordersSearchModal');
+          site.hideModal("#ordersSearchModal");
           $scope.search = {};
-
         }
       },
       function (err) {
         $scope.busy = false;
         $scope.error = err;
       }
-
-    )
+    );
   };
 
   $scope.getGovesList = function (where) {
@@ -184,27 +185,27 @@ app.controller("orders", function ($scope, $http, $timeout) {
       url: "/api/gov",
       data: {
         where: {
-          active: true
+          active: true,
         },
         select: {
-          id: 1, name_ar: 1, name_en: 1
-        }
-      }
+          id: 1,
+          name_ar: 1,
+          name_en: 1,
+        },
+      },
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.docs.length > 0) {
           $scope.govesList = response.data.docs;
-
         }
       },
       function (err) {
         $scope.busy = false;
         $scope.error = err;
       }
-    )
+    );
   };
-
 
   $scope.getUsersList = function (where) {
     $scope.busy = true;
@@ -213,35 +214,23 @@ app.controller("orders", function ($scope, $http, $timeout) {
       url: "/api/patients",
       data: {
         where: {
-          active: true
+          active: true,
         },
-       
-      }
+      },
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.docs.length > 0) {
           $scope.userList = response.data.docs;
-
         }
       },
       function (err) {
         $scope.busy = false;
         $scope.error = err;
       }
-    )
+    );
   };
 
-
-
-
-
-
-
-
-
-  
-  
   $scope.getCitiesList = function (where) {
     $scope.busy = true;
     $http({
@@ -249,25 +238,26 @@ app.controller("orders", function ($scope, $http, $timeout) {
       url: "/api/city",
       data: {
         where: {
-          active: true
+          active: true,
         },
         select: {
-          id: 1, name_ar: 1, name_en: 1
-        }
-      }
+          id: 1,
+          name_ar: 1,
+          name_en: 1,
+        },
+      },
     }).then(
       function (response) {
         $scope.busy = false;
         if (response.data.docs.length > 0) {
           $scope.citiesList = response.data.docs;
-
         }
       },
       function (err) {
         $scope.busy = false;
         $scope.error = err;
       }
-    )
+    );
   };
   $scope.getInsuranceCompanyList = function (where) {
     $scope.busy = true;
@@ -276,12 +266,16 @@ app.controller("orders", function ($scope, $http, $timeout) {
       url: "/api/insuranceCompany",
       data: {
         where: {
-          active: true
+          active: true,
         },
         select: {
-          id: 1, name_ar: 1, name_en: 1, balance: 1, image: 1
-        }
-      }
+          id: 1,
+          name_ar: 1,
+          name_en: 1,
+          balance: 1,
+          image: 1,
+        },
+      },
     }).then(
       function (response) {
         $scope.busy = false;
@@ -293,41 +287,13 @@ app.controller("orders", function ($scope, $http, $timeout) {
         $scope.busy = false;
         $scope.error = err;
       }
-    )
+    );
   };
-  $scope.addImgArr = function () {
-
-    $scope.orders.image= $scope.orders.image || [];
-
-    $scope.orders.image.push($scope.prescription);
-    $scope.prescription = {};
-  };
-
-  $scope.next = function () {
-
-    site.showModal('#personalData');
-  
-  };
-
-  $scope.displaySearchModal = function () {
-    $scope.error = '';
-    site.showModal('#ordersSearchModal');
-
-  };
-
-  $scope.searchAll = function () {
-
-    $scope.getOrdersList($scope.search);
-    site.hideModal('#ordersSearchModal');
-    $scope.search = {};
-  };
-  
 
   $scope.getOrdersList();
   $scope.getGovesList();
   $scope.getCitiesList();
   $scope.getUsersList();
-  
-  
+
   $scope.getInsuranceCompanyList();
 });
